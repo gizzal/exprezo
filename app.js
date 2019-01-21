d0abb07b2bc1e3721ce24102eb070a9f21a93fe6
Carlos Rojas <c.rojas@portavita.ru>
2019-01-21 17:18:25 +0300

    other commit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express =require('express');
 const app = express();
+var port = process.env.PORT || 3000;
 var welcome = 'Welcome to the server'
 const colors = [
   'red',
@@ -13,4 +14,4 @@ app.set('view engine','pug');
 app.get('/',(req,res)=>{
   res.render('index', {colors,welcome});
 });
-app.listen(3000);
+app.listen(port);

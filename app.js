868b9b8fa48725f26514c4c87c2429ceb141c1e1
Carlos Rojas <c.rojas@portavita.ru>
2019-01-22 18:59:00 +0300

    added body parser

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express =require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.set('view engine','pug');
+
 var port = process.env.PORT || 3000;
 var welcome = 'Welcome to the server'
 const colors = [
@@ -10,8 +17,12 @@ const colors = [
   'blue',
   'purple'
 ];
-app.set('view engine','pug');
+
 app.get('/',(req,res)=>{
+  res.render('index', {colors,welcome});
+});
+app.post('/hello',(req,res)=>{
+  console.dir(req);
   res.render('index', {colors,welcome});
 });
 app.listen(port);

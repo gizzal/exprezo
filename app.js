45870aac5fe1b155187a876324f5d374f351bd59
home No To ri <soy_solamente@hotmail.com>
2019-02-09 21:58:03 +0300

    added cookies

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express =require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended:false}));
+app.use(cookieParser());
 
 app.set('view engine','pug');
 
@@ -19,10 +21,16 @@ const colors = [
 ];
 
 app.get('/',(req,res)=>{
+  res.clearCookie('username');
   res.render('index', {colors,welcome});
 });
-app.post('/hello',(req,res)=>{
-  console.dir(req);
-  res.json(req.body);
+app.get('/hello',(req,res)=>{
+  res.render('index', {colors, welcome, name: req.cookies.username});
 });
-app.listen(port);
+app.post('/hello',(req,res)=>{
+  //console.dir(req);
+  //res.json(req.body);
+  res.cookie('username',req.body.username)
+  res.redirect("/hello");
+});
+app.listen(port,()=>console.log("You are connected to port "+port));
